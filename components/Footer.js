a64465be75b84e22729d0e7e3a3982d3f4b0e968
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-12 00:06:45 +0200

    fix strings

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <div className="container">
-    <p>If you're interesting to work with me or just want to say me "Hello" drop me a message to <a href="mailto:ademir.besirovic1996@gmail.com">ademir.besirovic1996@gmail.com</a></p>
-    <p>Need more information about me, my working experience or skills ... <a href="./static/ademir-besirovic.pdf" target="_blank">download my CV</a></p>
+    <p>If you're interesting to work with me or just want to say "Hello" drop me a message to <a href="mailto:ademir.besirovic1996@gmail.com">ademir.besirovic1996@gmail.com</a>.</p>
+    <p>Need more information about me, my working experience or skills ? ... <a href="./static/ademir-besirovic.pdf" target="_blank">download my CV</a></p>
     <style jsx>{`
       div {
         border-top: 1px solid #444;
