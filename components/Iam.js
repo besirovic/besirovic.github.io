545168a927f6ddd4efc5fe3bfc9ce2ccd0ab2cae
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-05-16 18:19:23 +0200

    add missing space after comma

--- components/Iam.js
+++ components/Iam.js
@@ -2,7 +2,7 @@ const Iam = () => (
   <div style={{ maxWidth: 1200 }}>
     <p>Hello, I'm</p>
     <h1>Ademir Beširović</h1>
-    <p>22 y/o, self-taught,full-stack software developer who loves to work with JavaScript/TypeScript, React, Node, GoLang, GraphQL and any other modern technology.</p>
+    <p>22 y/o, self-taught, full-stack software developer who loves to work with JavaScript/TypeScript, React, Node, GoLang, GraphQL and any other modern technology.</p>
     <style jsx>{`
       h1 {
         color: #50e3c2;
